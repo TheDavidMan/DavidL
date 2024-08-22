d4ffc2c5b5e5a956a6a08993f38af19467548243
DavidL <dlopera@cesde.net>
2024-08-21 22:30:31 -0500

    Solucion ejercicio 5

---
+++ Ejercicio5.js
@@ -0,0 +1,15 @@
+let Num1 = prompt("Ingrese un numero ")
+
+Num1 = parseInt(Num1)
+
+let Num2 = prompt("Ingrese un numero ")
+
+Num2 = parseInt(Num2)
+
+if(Num1 > Num2){
+    console.log("El Num1 es mayor que el Num2 ")
+} else if(Num1 < Num2){
+    console.log("El Num1 es menor que el Num2 ")
+} else if(Num1 == Num2){
+    console.log("Los numeros son iguales")
+}
